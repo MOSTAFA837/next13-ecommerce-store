f42d9b639e9318450458c1022125b528e6eca8f6
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-12-23 22:01:20 +0200

    show billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
+import getBillboard from "@/actions/get-billboard";
+import billboard from "@/components/billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
 import React from "react";
 
-export default function Home() {
-  return <div>Home</div>;
+export const revalidate = 0;
+
+export default async function Home() {
+  const billboard = await getBillboard("0cfbfae6-6483-447c-bdb6-ec9f6487a400");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 }
